164eb6faf4f8b8cf467706c6c70a03868cb1af06
heyjon1984 <heyjon1984@gmail.com>
2022-08-29 17:56:27 +0900

    오더/프로덕트/리뷰 vo

---
+++ nonasaja/src/main/java/kr/spring/order/vo/OrderVO.java
@@ -0,0 +1,174 @@
+package kr.spring.order.vo;
+
+import java.sql.Date;
+
+import javax.validation.constraints.NotEmpty;
+
+public class OrderVO {
+	// order 테이블
+	@NotEmpty
+	private int order_num;
+	@NotEmpty
+	private int mem_num;
+	//주문상품 전체 이름
+	@NotEmpty
+	private String total_name;
+	@NotEmpty
+	private int order_total;
+	//1-배송대기,2-배송준비,3-배송중,4-배송완료,5-주문취소
+	@NotEmpty
+	private int status;
+	@NotEmpty
+	private String receive_name;
+	@NotEmpty
+	private String receive_post;
+	@NotEmpty
+	private String receive_address1;
+	@NotEmpty
+	private String receive_address2;
+	@NotEmpty
+	private String receive_phone;
+	private String notice;
+	@NotEmpty
+	private Date reg_date;
+	private Date modify_date;
+	
+	//order_detail 테이블
+	@NotEmpty
+	private int detail_num;
+	@NotEmpty 
+	private int product_num;
+	@NotEmpty
+	private String product_name;
+	@NotEmpty
+	private int product_price;
+	@NotEmpty
+	private int product_total;
+	@NotEmpty
+	private int order_quantity;
+	
+	public int getOrder_num() {
+		return order_num;
+	}
+	public void setOrder_num(int order_num) {
+		this.order_num = order_num;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	public String getTotal_name() {
+		return total_name;
+	}
+	public void setTotal_name(String total_name) {
+		this.total_name = total_name;
+	}
+	public int getOrder_total() {
+		return order_total;
+	}
+	public void setOrder_total(int order_total) {
+		this.order_total = order_total;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getReceive_name() {
+		return receive_name;
+	}
+	public void setReceive_name(String receive_name) {
+		this.receive_name = receive_name;
+	}
+	public String getReceive_post() {
+		return receive_post;
+	}
+	public void setReceive_post(String receive_post) {
+		this.receive_post = receive_post;
+	}
+	public String getReceive_address1() {
+		return receive_address1;
+	}
+	public void setReceive_address1(String receive_address1) {
+		this.receive_address1 = receive_address1;
+	}
+	public String getReceive_address2() {
+		return receive_address2;
+	}
+	public void setReceive_address2(String receive_address2) {
+		this.receive_address2 = receive_address2;
+	}
+	public String getReceive_phone() {
+		return receive_phone;
+	}
+	public void setReceive_phone(String receive_phone) {
+		this.receive_phone = receive_phone;
+	}
+	public String getNotice() {
+		return notice;
+	}
+	public void setNotice(String notice) {
+		this.notice = notice;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public Date getModify_date() {
+		return modify_date;
+	}
+	public void setModify_date(Date modify_date) {
+		this.modify_date = modify_date;
+	}
+	public int getDetail_num() {
+		return detail_num;
+	}
+	public void setDetail_num(int detail_num) {
+		this.detail_num = detail_num;
+	}
+	public int getProduct_num() {
+		return product_num;
+	}
+	public void setProduct_num(int product_num) {
+		this.product_num = product_num;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public int getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(int product_price) {
+		this.product_price = product_price;
+	}
+	public int getProduct_total() {
+		return product_total;
+	}
+	public void setProduct_total(int product_total) {
+		this.product_total = product_total;
+	}
+	public int getOrder_quantity() {
+		return order_quantity;
+	}
+	public void setOrder_quantity(int order_quantity) {
+		this.order_quantity = order_quantity;
+	}
+	@Override
+	public String toString() {
+		return "OrderVO [order_num=" + order_num + ", mem_num=" + mem_num + ", total_name=" + total_name
+				+ ", order_total=" + order_total + ", status=" + status + ", receive_name=" + receive_name
+				+ ", receive_post=" + receive_post + ", receive_address1=" + receive_address1 + ", receive_address2="
+				+ receive_address2 + ", receive_phone=" + receive_phone + ", notice=" + notice + ", reg_date="
+				+ reg_date + ", modify_date=" + modify_date + ", detail_num=" + detail_num + ", product_num="
+				+ product_num + ", product_name=" + product_name + ", product_price=" + product_price
+				+ ", product_total=" + product_total + ", order_quantity=" + order_quantity + "]";
+	}
+}
---
+++ nonasaja/src/main/java/kr/spring/product/vo/ProductVO.java
@@ -0,0 +1,208 @@
+package kr.spring.product.vo;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ProductVO {
+	@NotEmpty
+	private int product_num;
+	@NotEmpty
+	private String name;
+	@NotEmpty
+	private int kind;
+	@NotEmpty
+	private String sub_category;
+	@NotEmpty
+	private String title;
+	//원가
+	@NotEmpty
+	private int price1; 
+	//판매가
+	@NotEmpty
+	private int price2;
+	@NotEmpty
+	private int quantity;
+	//구매요구수량
+	@NotEmpty
+	private int req_quantity;
+	//대표이미지 (나머지 둘은 슬라이드)
+	@NotEmpty
+	private byte[] photo1;
+	private byte[] photo2;
+	private byte[] photo3;
+	@NotEmpty
+	private String photo1_name;
+	private String photo2_name;
+	private String photo3_name;
+	@NotEmpty
+	private String company;
+	@NotEmpty
+	private String origin;
+	//0:판매중 1:마감
+	@NotEmpty
+	private int status;
+	@NotEmpty
+	private Date deadline;
+	@NotEmpty
+	private Date reg_date;
+	private Date modify_date;
+	
+	//이미지 blob 처리
+	//주의! 사진 업로드 폼에서 파일 업로드 파라미터네임은 반드시 upload라고 지정해야 함
+	public void setUpload1(MultipartFile upload) throws IOException{
+		//MultipartFile -> byte[]로 형변환
+		setPhoto1(upload.getBytes());
+		//파일 이름 처리
+		setPhoto1_name(upload.getOriginalFilename());
+	}
+	public void setUpload2(MultipartFile upload) throws IOException{
+		setPhoto2(upload.getBytes());
+		setPhoto2_name(upload.getOriginalFilename());
+	}
+	
+	public void setUpload3(MultipartFile upload) throws IOException{
+		setPhoto3(upload.getBytes());
+		setPhoto3_name(upload.getOriginalFilename());
+	}
+	
+	public int getProduct_num() {
+		return product_num;
+	}
+	public void setProduct_num(int product_num) {
+		this.product_num = product_num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKind() {
+		return kind;
+	}
+	public void setKind(int kind) {
+		this.kind = kind;
+	}
+	public String getSub_category() {
+		return sub_category;
+	}
+	public void setSub_category(String sub_category) {
+		this.sub_category = sub_category;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPrice1() {
+		return price1;
+	}
+	public void setPrice1(int price1) {
+		this.price1 = price1;
+	}
+	public int getPrice2() {
+		return price2;
+	}
+	public void setPrice2(int price2) {
+		this.price2 = price2;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getReq_quantity() {
+		return req_quantity;
+	}
+	public void setReq_quantity(int req_quantity) {
+		this.req_quantity = req_quantity;
+	}
+	public byte[] getPhoto1() {
+		return photo1;
+	}
+	public void setPhoto1(byte[] photo1) {
+		this.photo1 = photo1;
+	}
+	public byte[] getPhoto2() {
+		return photo2;
+	}
+	public void setPhoto2(byte[] photo2) {
+		this.photo2 = photo2;
+	}
+	public byte[] getPhoto3() {
+		return photo3;
+	}
+	public void setPhoto3(byte[] photo3) {
+		this.photo3 = photo3;
+	}
+	public String getPhoto1_name() {
+		return photo1_name;
+	}
+	public void setPhoto1_name(String photo1_name) {
+		this.photo1_name = photo1_name;
+	}
+	public String getPhoto2_name() {
+		return photo2_name;
+	}
+	public void setPhoto2_name(String photo2_name) {
+		this.photo2_name = photo2_name;
+	}
+	public String getPhoto3_name() {
+		return photo3_name;
+	}
+	public void setPhoto3_name(String photo3_name) {
+		this.photo3_name = photo3_name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Date getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public Date getModify_date() {
+		return modify_date;
+	}
+	public void setModify_date(Date modify_date) {
+		this.modify_date = modify_date;
+	}
+	@Override
+	public String toString() {
+		return "ProductVO [product_num=" + product_num + ", name=" + name + ", kind=" + kind + ", sub_category="
+				+ sub_category + ", title=" + title + ", price1=" + price1 + ", price2=" + price2 + ", quantity="
+				+ quantity + ", req_quantity=" + req_quantity + ", photo1_name=" + photo1_name + ", photo2_name="
+				+ photo2_name + ", photo3_name=" + photo3_name + ", company=" + company + ", origin=" + origin
+				+ ", status=" + status + ", deadline=" + deadline + ", reg_date=" + reg_date + ", modify_date="
+				+ modify_date + "]";
+	}
+	
+}
