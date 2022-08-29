570abcf1a97ac456d7d28daca986dc832b691538
easyp <easyp@DESKTOP-K1N31VV>
2022-08-29 15:43:29 +0900

    중고거래 주석 완료

--- nonasaja/src/main/webapp/sql/table_lee.sql
+++ nonasaja/src/main/webapp/sql/table_lee.sql
@@ -3,10 +3,10 @@ create table used_board(
  used_num number not null,
  title varchar2(150) not null, 
  content clob not null,
- kind number(1) not null,
+ kind number(1) not null, --거래 종류(1중고,2무나,3교환)
  price number(6) default 0 not null,
- status number(1) not null,
- category varchar2(15) not null,
+ status number(1) not null,--거래상태(1판매중/2판매완료)
+ category varchar2(15) not null,--상품 카테고리(주방,생활,식품용품)
  trade varchar2(30),
  reg_date date not null,
  modify_date date,
