f9af44c199525ccff8fb6fa0b819b4a7e3ec43fe
heyjon1984 <heyjon1984@gmail.com>
2022-09-22 00:40:31 +0900

    상품 리스트/디테일 UI

--- nonasaja/src/main/resources/static/js/product.detail.js
+++ nonasaja/src/main/resources/static/js/product.detail.js
@@ -2,7 +2,7 @@ $(function(){
 	//==========주문 수량 변경==============
 	$('#order_quantity').on('keyup mouseup',function(){
 		if($('#order_quantity').val()==''){
-			$('#product_total_txt').text('총 주문 금액 : 0원');
+			$('#product_total_txt').text('0원');
 			return;
 		}
 		if($('#order_quantity').val() <= 0){
@@ -12,14 +12,20 @@ $(function(){
 		if(Number($('#product_quantity').val()) < $('#order_quantity').val()){
 			alert('수량이 부족합니다');
 			$('#order_quantity').val('');
-			$('#product_total_txt').text('총 주문 금액 : 0원');
+			$('#product_total_txt').text('0원');
 			return;
 		}
 		
 		let total = $('#product_price2').val() * $('#order_quantity').val();
-		$('#product_total_txt').text('총주문 금액 : ' +$.number(total) +'원');
+		$('#product_total_txt').text($.number(total) +'원');
 	});
 	
+	$('#product_cart').submit(function(){
+		if($('#order_quantity').val() <= 0){
+			alert('주문 수량을 입력하세요');
+			return false;
+		}
+	});
 });
 
 
