5ef89103a0e474de2844395a241753af1d673e7e
yuntaekOhO <oyt1996@naver.com>
2022-09-26 20:46:57 +0900

    카카오페이 수정

--- nonasaja/src/main/java/kr/spring/kakaopay/vo/ReadyResponse.java
+++ nonasaja/src/main/java/kr/spring/kakaopay/vo/ReadyResponse.java
@@ -1,5 +1,7 @@
 package kr.spring.kakaopay.vo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,6 +9,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ReadyResponse {
 	private String tid; //결제 고유 번호, 20자
 	private boolean tms_result; //?
